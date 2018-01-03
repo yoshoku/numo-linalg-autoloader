bab184b4d212502c4b375fbef0dcf43e8e16c8a7
yoshoku <yoshoku@outlook.com>
2018-01-03 19:29:11 +0900

    :sparkles: add test cases about BLAS/LAPACK methods

--- spec/numo-linalg-autoloader_spec.rb
+++ spec/numo-linalg-autoloader_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Numo::Linalg::Autoloader do
     expect { described_class.load_library }.to_not raise_error
     expect(described_class.libs).to_not be_nil
   end
+
+  describe 'BLAS/LAPACK methods' do
+    let(:mat) { Numo::DFloat.new(5,3).rand }
+    before { described_class.load_library }
+
+    it 'can perform matrix multiplication' do
+      expect { mat.dot(mat.transpose) }.to_not raise_error
+    end
+
+    it 'can perform singular value decomposition' do
+      expect { Numo::Linalg::svd(mat) }.to_not raise_error
+    end
+  end
 end
