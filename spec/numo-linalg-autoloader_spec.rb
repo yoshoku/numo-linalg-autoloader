d6850cd67affeef9ca6279d1767c8159768e511c
yoshoku <yoshoku@outlook.com>
2018-01-07 01:25:05 +0900

    :u5408: add test cases for methods finding backend libraries

--- spec/numo-linalg-autoloader_spec.rb
+++ spec/numo-linalg-autoloader_spec.rb
@@ -6,6 +6,36 @@ RSpec.describe Numo::Linalg::Autoloader do
     expect(described_class.libs).to_not be_nil
   end
 
+  describe 'methods finding backend libraries' do
+    let(:autoloader) do
+      class DummyLoader
+        include Numo::Linalg::Autoloader
+      end
+      DummyLoader.new
+    end
+
+    it 'finds OpenBLAS libraries' do
+      openblas_libs = autoloader.send(:find_openblas_libs, ['/usr/local/opt/openblas/lib'])
+      expect(openblas_libs[:openblas]).to_not be_nil
+      expect(openblas_libs[:lapacke]).to_not be_nil
+    end
+
+    it 'finds ATLAS libraries' do
+      atlas_libs = autoloader.send(:find_atlas_libs, ['/opt/local/lib'])
+      expect(atlas_libs[:atlas]).to_not be_nil
+      expect(atlas_libs[:cblas]).to_not be_nil
+      expect(atlas_libs[:lapacke]).to_not be_nil
+    end
+
+    it 'finds BLAS/LAPACK libraries' do
+      lapack_libs = autoloader.send(:find_lapack_libs, ['/usr/lib', '/usr/local/opt/lapack/lib'])
+      expect(lapack_libs[:blas]).to_not be_nil
+      expect(lapack_libs[:cblas]).to_not be_nil
+      expect(lapack_libs[:lapack]).to_not be_nil
+      expect(lapack_libs[:lapacke]).to_not be_nil
+    end
+  end
+
   describe 'BLAS/LAPACK methods' do
     let(:mat) { Numo::DFloat.new(5,3).rand }
     before { described_class.load_library }
